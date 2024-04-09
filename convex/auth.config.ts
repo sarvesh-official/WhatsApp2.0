ffc6e9917723746596415259f039787b33aa1327
Sarvesh <sarveshjaiofficial@gmail.com>
2024-04-09 13:57:57 +0530

    Implemented Webhooks

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://right-glider-76.clerk.accounts.dev/",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
